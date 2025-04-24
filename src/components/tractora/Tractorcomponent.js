e071f992f9876fc3b3249b3032a7f45786fc330a
wasd21xd <Avey21703@gmail.com>
2025-04-24 12:11:25 +0300

    доделал блок в гридах, сделал стили с макета там же

--- src/components/tractora/Tractorcomponent.js
+++ src/components/tractora/Tractorcomponent.js
@@ -3,6 +3,7 @@ import './tractori.css'
 import traktor4iki from "./Tractori";
 import ForkliftCard from "./card";
 import ForkliftModal from "./forliftModal";
+import catalog from '../../components/tractora/img__tractor/catalog.png'
 
 import React, { useState } from 'react';
 
@@ -10,12 +11,19 @@ function TractorComponent () {
     const [selectedForklift, setSelectedForklift] = useState(null);
 
     return (
-        <div className="catalog">
-            <h1>Каталог погрузчиков</h1>
+        <div className="catalog__container">
+            <h1>Каталог <span className='span__h1'>погрузчиков</span></h1>
             <div className="grid">
                 {traktor4iki.map(f => (
                     <ForkliftCard key={f.id} forklift={f} onClick={setSelectedForklift}/>
                 ))}
+                <div className="grid-placeholder">
+                    <img className='img__catalog' src={catalog} alt="кфтфлог"/>
+                    <div className='right__catalog'>
+                        <h2 className='logo__catalog'>Интересует дизельный  или газобензиновый погрузчик?</h2>
+                        <button className='btn__catalog'>Получить прайс-лист</button>
+                    </div>
+                </div>
             </div>
             <ForkliftModal forklift={selectedForklift} onClose={() => setSelectedForklift(null)}/>
         </div>
--- src/components/tractora/card.js
+++ src/components/tractora/card.js
@@ -1,13 +1,12 @@
 const ForkliftCard = ({ forklift, onClick }) => (
     <div className="card" onClick={() => onClick(forklift)}>
-        <img src={forklift.image} alt={forklift.name} />
-        <h3>{forklift.name}</h3>
-        <p>Тип двигателя: {forklift.type}</p>
-        <p>Грузоподъемность: {forklift.capacity}</p>
-        <p>Высота подъема: {forklift.height}</p>
-        <button>Получить предложение</button>
-        <a>Подробные характеристики</a>
+            <img className='img__card__tractor' src={forklift.image} alt={forklift.name} />
+            <h3 className='name__card highlight-text'>{forklift.name}</h3>
+            <p>Тип двигателя: <span className="highlight-text">{forklift.type}</span></p>
+            <p>Грузоподъемность: <span className="highlight-text">{forklift.capacity}</span></p>
+            <p>Высота подъема: <span className="highlight-text">{forklift.height}</span></p>
+            <button>Получить предложение</button>
+            <a>Подробные характеристики</a>
     </div>
 );
-
 export default ForkliftCard;
